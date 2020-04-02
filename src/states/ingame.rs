23f2ae5d54e19024034284a8fd25d0c2b7c247c7
lberrymage <mageelog@gmail.com>
2020-04-01 17:12:14 -0800

    Obey new linting rules
    This consists mainly of two tasks:
    
     - Document all (private) items
    
     - Limit the scope of `pub` exports and use declarations

--- src/states/ingame.rs
+++ src/states/ingame.rs
@@ -1,6 +1,9 @@
+//! The main gameplay state
+
 use super::state_prelude::*;
 
-pub struct Ingame;
+/// The struct implementing the main gameplay state
+pub(crate) struct Ingame;
 
 impl SimpleState for Ingame {
     fn on_start(&mut self, _data: StateData<'_, GameData<'_, '_>>) {}
--- src/states/mod.rs
+++ src/states/mod.rs
@@ -1,7 +1,12 @@
-pub mod state_prelude {
-    pub use amethyst::{GameData, SimpleState, StateData};
+//! The collection of possible game states
+
+/// Contains common types used when implememnting a [`State`]
+///
+/// [`State`]: https://docs.amethyst.rs/stable/amethyst/trait.State.html
+mod state_prelude {
+    pub(super) use amethyst::{GameData, SimpleState, StateData};
 }
 
-pub use ingame::Ingame;
+pub(crate) use ingame::Ingame;
 
 mod ingame;
